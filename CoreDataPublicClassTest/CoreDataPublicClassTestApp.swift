7f91b42aeb448b7984e75185d6454abfb4d88951
pkrakow <pkrakow@gmail.com>
2021-01-25 17:28:26 -0800

    Initial Commit

---
+++ CoreDataPublicClassTest/CoreDataPublicClassTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataPublicClassTestApp.swift
+//  CoreDataPublicClassTest
+//
+//  Created by Paul Krakow on 1/25/21.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataPublicClassTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
